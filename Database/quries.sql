7ed9cdda47f02620bd7776a558e8963d912ab5f8
ghous-mohammad <ghosmohammad247@gmail.com>
2023-08-30 10:05:14 +0500

    sql quries syntax change

--- Database/quries.sql
+++ Database/quries.sql
@@ -1,14 +1,13 @@
 CREATE TABLE IF NOT EXISTS cartData (
-
-    id SERIAL ,
+    id SERIAL,
     user_id varchar(255) NOT NULL,
-    product_id varchar(255) Not NULL,
-    product_title varchar(255) Not NULL,
-    product_price init Not NULL,
-    product_quantity init NOT NULL,
+    product_id varchar(255) NOT NULL,
+    product_title varchar(255) NOT NULL,
+    product_price int NOT NULL,
+    product_quantity int NOT NULL,
     image_url text NOT NULL
-
 )
+
 
 
 ALTER TABLE data 
